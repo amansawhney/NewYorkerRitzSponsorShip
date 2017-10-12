39b579394e457fa2a896b38520c1bf8907c7555e
amansawhney <amansawhney@sawhneygames.com>
2017-10-12 11:46:34 -0400

    fixed flashing images

--- index.js
+++ index.js
@@ -1,15 +1,205 @@
-$(window).scroll(function(e) {
+var counter = 0;
+var changed = false;
 
-    var 	r = $(window).scrollTop(),
-        t = $(".container-hero").height();
+jQuery(function($) {
+  init();
+}); // start jQuery when DOM is ready
 
-    // if container hero is less than window.scrollTop
-    t > r && (
-        $("header .wrapper").css({
-            top: .4 * r
-        }),
-            $("header .wrapper").css({
-                opacity: (300 - r) / 100
-            })
-    )
-});
+function init() {
+  // start
+  setWindow(); // set up window events
+}
+
+function setWindow() {
+  $(window).on('load resize scroll', function(e) {
+    // when window is ready, resized, or scrolled
+    viewportLog(); // log view counter
+  });
+}
+
+function viewportLog() {
+  // log view counter
+  id = '';
+  if (counter === 0) {
+    id = '#mydivname';
+  } else if (counter === 1) {
+    id = '#mydivname2';
+  } else if (counter === 2) {
+    id = '#mydivname3';
+  } else if (counter === 3) {
+    id = '#mydivname4';
+  } else if (counter === 4) {
+    id = '#mydivname5';
+  } else if (counter === 5) {
+    id = '#mydivname6';
+  } else if (counter === 6) {
+    id = '#mydivname7';
+  }
+  if (
+    topInViewport($('#mydivname')) &&
+    $('#elem').css('background-image').indexOf('image2_@2x.jpg') === -1
+  ) {
+    console.log(
+      $('#elem').css('background-image').indexOf('image2_@2x.jpg') === -1,
+    );
+    newUrl = "'./Assets for Developer 10-10-17/image2_@2x.jpg'";
+    if (
+      !changed &&
+      $('#elem').css('background-image').indexOf('image2_@2x.jpg') === -1
+    ) {
+      changed = true;
+      $('#elem')
+        .stop(true).fadeTo('slow', 0, function() {
+          $(this).css('background-image', 'url(' + newUrl + ')');
+          console.log('1');
+        })
+        .fadeTo('slow', 1);
+      return;
+    }
+  } else if (
+    topInViewport($('#mydivname2')) &&
+    $('#elem').css('background-image').indexOf('image2_wQuote_@2x.jpg') === -1
+  ) {
+    console.log(
+      $('#elem').css('background-image').indexOf('image2_wQuote_@2x.jpg') ===
+        -1,
+    );
+
+    newUrl = "'./Assets for Developer 10-10-17/image2_wQuote_@2x.jpg'";
+    if (
+      !changed &&
+      $('#elem').css('background-image').indexOf('image2_wQuote_@2x.jpg') === -1
+    ) {
+      changed = true;
+      $('#elem')
+        .stop(true).fadeTo('slow', 0, function() {
+          $(this).css('background-image', 'url(' + newUrl + ')');
+          console.log('2');
+        })
+        .fadeTo('slow', 1);
+      return;
+    }
+  } else if (
+    topInViewport($('#mydivname3')) &&
+    $('#elem').css('background-image').indexOf('image3_@2x.jpg') === -1
+  ) {
+    console.log(
+      $('#elem').css('background-image').indexOf('image3_@2x.jpg') === -1,
+    );
+
+    newUrl = "'./Assets for Developer 10-10-17/image3_@2x.jpg'";
+    if (
+      !changed &&
+      $('#elem').css('background-image').indexOf('image3_@2x.jpg') === -1
+    ) {
+      changed = true;
+      $('#elem')
+        .stop(true).fadeTo('slow', 0, function() {
+          $(this).css('background-image', 'url(' + newUrl + ')');
+          console.log('3');
+        })
+        .fadeTo('slow', 1);
+      return;
+    }
+  } else if (
+    topInViewport($('#mydivname4')) &&
+    $('#elem').css('background-image').indexOf('image4_@2x.jpg') === -1
+  ) {
+    console.log(
+      $('#elem').css('background-image').indexOf('image4_@2x.jpg') === -1,
+    );
+
+    newUrl = "'./Assets for Developer 10-10-17/image4_@2x.jpg'";
+    if (
+      !changed &&
+      $('#elem').css('background-image').indexOf('image4_@2x.jpg') === -1
+    ) {
+      changed = true;
+      $('#elem')
+        .stop(true).fadeTo('slow', 0, function() {
+          $(this).css('background-image', 'url(' + newUrl + ')');
+          console.log('4');
+        })
+        .fadeTo('slow', 1);
+      return;
+    }
+  } else if (
+    topInViewport($('#mydivname5')) &&
+    $('#elem').css('background-image').indexOf('image4_wQuote_@2x.jpg') === -1
+  ) {
+    console.log(
+      $('#elem').css('background-image').indexOf('image4_wQuote_@2x.jpg') ===
+        -1,
+    );
+
+    newUrl = "'./Assets for Developer 10-10-17/image4_wQuote_@2x.jpg'";
+    if (
+      !changed &&
+      $('#elem').css('background-image').indexOf('image4_wQuote_@2x.jpg') === -1
+    ) {
+      changed = true;
+      $('#elem')
+        .stop(true).fadeTo('slow', 0, function() {
+          $(this).css('background-image', 'url(' + newUrl + ')');
+          console.log('5');
+        })
+        .fadeTo('slow', 1);
+      return;
+    }
+  } else if (
+    topInViewport($('#mydivname6')) &&
+    $('#elem').css('background-image').indexOf('image5_@2x.jpg') === -1
+  ) {
+    console.log(
+      $('#elem').css('background-image').indexOf('image5_@2x.jpg') === -1,
+    );
+
+    newUrl = "'./Assets for Developer 10-10-17/image5_@2x.jpg'";
+    if (
+      !changed &&
+      $('#elem').css('background-image').indexOf('image5_@2x.jpg') === -1
+    ) {
+      changed = true;
+      $('#elem')
+        .stop(true).fadeTo('slow', 0, function() {
+          $(this).css('background-image', 'url(' + newUrl + ')');
+          console.log('6');
+        })
+        .fadeTo('slow', 1);
+      return;
+    }
+  } else if (
+    topInViewport($('#mydivname7')) &&
+    $('#elem').css('background-image').indexOf('image5_wQuote_@2x.jpg') === -1
+  ) {
+    console.log(
+      $('#elem').css('background-image').indexOf('image5_wQuote_@2x.jpg') ===
+        -1,
+    );
+
+    newUrl = "'./Assets for Developer 10-10-17/image5_wQuote_@2x.jpg'";
+    if (
+      !changed &&
+      $('#elem').css('background-image').indexOf('image5_wQuote_@2x.jpg') === -1
+    ) {
+      changed = true;
+      $('#elem')
+        .stop(true).fadeTo('slow', 0, function() {
+          $(this).css('background-image', 'url(' + newUrl + ')');
+          console.log('7');
+        })
+        .fadeTo('slow', 1);
+      return;
+    }
+  } else {
+    changed = false;
+  }
+}
+
+function topInViewport(element) {
+  // is the top of the element in viewport?
+  return (
+    $(element).offset().top >= $(window).scrollTop() &&
+    $(element).offset().top <= $(window).scrollTop() + $(window).height()
+  );
+}
